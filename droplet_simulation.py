3faf38ba748c53f6f317dfc02872c4a99ea0ceb3
Oliver <odressler@gmail.com>
2013-05-01 01:26:50 +0200

    Added Readme

---
+++ droplet_simulation.py
@@ -0,0 +1,302 @@
+import random
+import math
+import csv
+import itertools as it
+from collections import defaultdict
+
+# Droplet is built up as follows:
+# droplet = [volume, content]
+# content = {'molecule_name1': amount, 'molecule_name2': amount2, ...}
+
+
+def stream(content={}, volume=0, volume_sigma=0, content_sigma=0):
+    '''Basic stream of droplets; Unlimited supply of one type of droplet'''
+    while 1:
+        vol = random.gauss(volume, volume_sigma*volume)
+        if content_sigma == 0:
+            cont = content.copy()
+        else:
+            cont = {}
+            for mol, amount in content.items():
+                cont[mol] = random.gauss(amount, content_sigma*amount)
+
+        yield list([vol, cont])
+
+
+def combine(stream1, stream2):
+    '''samples alternatingly from two streams'''
+    one = True
+    while 1:
+        if one:
+            yield stream1.next()
+        else:
+            yield stream2.next()
+        one = not one
+
+
+def merge(stream1, stream2):
+    '''Merge droplets coming from two streams, 1:1'''
+    while 1:
+        d1 = stream1.next()
+        d2 = stream2.next()
+        new_volume = d1[0] + d2[0]
+        content = d1[1]
+
+        for molecule, amount in d2[1].items():
+            try:
+                content[molecule] += amount
+            except KeyError:
+                content[molecule] = amount
+
+        yield [new_volume, content.copy()]
+
+
+def split(stream):
+    '''Split droplets of stream in half (volume & content)'''
+    while 1:
+        droplet = stream.next()
+        new_volume = float(droplet[0]) / 2
+        content = {}
+        for c_id in droplet[1].keys():
+            new_amount = float(droplet[1][c_id]) / 2
+            content[c_id] = new_amount
+
+        # a split always leads to only one droplet
+        for i in range(2):
+            yield [new_volume, content.copy()]
+
+
+def reduce(stream, discart_ratio=0.5):
+    '''randomly discarts percentage of droplets'''
+    while 1:
+        while random.random < discart_ratio:
+            stream.next()
+        yield stream.next()
+
+
+def stream_buffer(stream, capacity=10):
+    '''Set up a buffer from which droplets are randomly sampled.'''
+
+    # picks a random droplet from the buffered droplets
+    # Fill buffer
+    buff = []
+    for i in range(capacity):
+        buff.append(stream.next())
+
+    while 1:
+        droplet = random.choice(buff)
+        yield droplet
+        buff.remove(droplet)
+        buff.append(stream.next())
+
+
+def copy_stream(stream):
+    '''Copy a stream'''
+    while 1:
+        yield stream.next()
+
+
+def sample(stream, num_samples=100):
+    '''Sample a number of droplets from the stream.'''
+    sampled = []
+    for i in range(num_samples):
+        sampled.append(stream.next())
+
+    return sampled
+
+
+def save_droplets(droplets, filename='droplets.csv'):
+    # Get all involved contents (dyes)
+    list_dyes = [content.keys() for v, content in droplets]
+    list_dyes = it.chain.from_iterable(list_dyes)
+    list_dyes = list(set(list_dyes))
+
+    # Save to csv
+    f = open(filename, 'wt')
+    try:
+        writer = csv.writer(f, delimiter='\t')
+        # set titles of columns
+        row = ['Volume'] + list_dyes
+        writer.writerow(tuple(row))
+
+        for volume, content in droplets:
+            templ = [volume] + [0] * len(list_dyes)
+            # write data in column
+            for mol, am in content.items():
+                ind = list_dyes.index(mol)
+                templ[ind+1] = am
+
+            writer.writerow(tuple(templ))
+
+    finally:
+        f.close()
+
+
+def filter_stream(stream, func, tries=500):
+    '''filter stream using a filter function (returns True if passes filter).'''
+    while 1:
+        found = False
+        for i in range(tries):
+            droplet = stream.next()
+            if func(droplet):
+                found = True
+                yield droplet
+                break
+
+        if not found:
+            raise RuntimeError("Filter couldn't find droplet after {0} tries.".format(tries))
+
+        yield droplet
+
+
+def multi_buffer(streams, capacity):
+    '''Buffer with multiple stream inputs. [streams], capacity'''
+    def stream_sel(streams):
+        while 1:
+            for stream in streams:
+                yield stream
+
+    buff = []
+
+    if type(streams) != list:
+        streams = [streams]
+
+    streams = stream_sel(streams)
+
+    while len(buff) < capacity:
+        buff.append(streams.next().next())
+
+    while 1:
+        droplet = random.choice(buff)
+        yield droplet
+        buff.remove(droplet)
+        buff.append(streams.next().next())
+
+
+def droplet_monitor(stream, name=''):
+    header = 'Monitor'
+    if name:
+        header += ' ' + name
+    header += ':\n'
+
+    while 1:
+        droplet = stream.next()
+        volume, content = droplet
+        disp = 'Volume: {}, '.format(round(volume, 3))
+        if content:
+            c = map(lambda cont: '{}: {}'.format(cont[0], round(cont[1], 3)), content.items())
+            disp += ', '.join(c)
+        else:
+            disp += 'no content'
+
+        print header + disp
+        yield droplet
+
+
+def hashify(droplets):
+    d = []
+    for vol, content in droplets:
+        c = []
+        for mol, am in content.items():
+            c.append((mol, am))
+        d.append((vol, tuple(c)))
+    return d
+
+
+def unique_droplets(droplets):
+    d = hashify(droplets)
+    unique = len(set(d))
+    d_len = len(droplets)
+    print '{}% unique droplets ({}/{})'.format(round(100.0*unique/d_len, 3), unique, d_len)
+
+
+def calculate_stdev(q):
+    '''From: http://www.daniweb.com/software-development/python/threads/438922/finding-the-standard-deviation-in-python'''
+    avg = float(sum(q))/len(q)
+    dev = []
+    for x in q:
+        dev.append(x - avg)
+    sqr = []
+    for x in dev:
+        sqr.append(x * x)
+    standard_dev = math.sqrt(sum(sqr)/(len(sqr)-1))
+    return standard_dev
+
+
+def extract_data(droplets, plot=False):
+    '''Create n-dimensional vectors, returned as a list
+    of lists; n = number unique of dyes in all droplets'''
+
+    # Get all involved contents (dyes)
+    dyes = it.chain.from_iterable([c.keys() for _, c in droplets])
+    dyes = list(set(dyes))
+
+    ret = defaultdict(list)
+    for v, cont in droplets:
+        for dye in dyes:
+            try:
+                d = cont[dye]/float(v)
+            except KeyError:
+                d = 0
+
+            ret[dye].append(d)
+
+    data = dict(ret)
+    if plot:
+        while len(data) < 3:
+            new_key = random.random()
+            data[new_key] = [0] * len(droplets)
+
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+
+        # Plot in scatterplot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        plt.title('Scatter plot of {0} droplets'.format(len(droplets)))
+        k = data.keys()[:3]
+        ax.scatter(data[k[0]], data[k[1]], data[k[2]],
+                   c='b', s=1, marker='.')
+        plt.show()
+
+    return data
+
+
+def histogram(droplets, bins=5):
+    '''Plot histogram of each droplet type.'''
+    import matplotlib.pyplot as plt
+
+    data = extract_data(droplets, False)
+    if data:
+        for k, v in data.items():
+            fig = plt.figure()
+            plt.title('Histogram for %s' % (k))
+            plt.hist(v, bins)
+
+        plt.show()
+
+# Some general filters for use with filter_stream (as second argument)
+
+
+def presence_filter(molecules):
+    '''Use this filter to extract all droplets containing all mentioned molecules.'''
+    def func(droplet):
+        k = droplet[1].keys()
+        if all([mol in k for mol in molecules]):
+            return True
+        else:
+            return False
+
+    return func
+
+
+def absence_filter(molecules):
+    '''Use this filter to extract all droplets not containing all mentioned molecules.'''
+
+    def func(droplet):
+        if any(mol in droplet[1].keys() for mol in molecules):
+            return False
+        else:
+            return True
+
+    return func
---
+++ stream_parser.py
@@ -0,0 +1,348 @@
+'''Parses a .streams file and returns a stream'''
+
+import droplet_simulation as dr
+import re
+
+
+class StreamParser(object):
+
+    def __init__(self, line=''):
+        # Streams is a dictionary of streams saved under a value (using ->)
+        # Droplets is a dictionary of lists of droplets saved under a value (using e.g. -1000->)
+        self.streams = {}
+        self.droplets = {}
+        self.snippets = {}
+
+        self.patterns = [
+            (r'\(([\d,.]*)\)\-\->(\w*)', 'CREATE_STREAM'),
+            (r'{([\w,.]+)}\-\->(\w*)', 'ADD_CONTENT'),
+            (r'(\w*)\-\->(\w*)', 'COPY'),
+
+            (r'(\w*)\-(\d+)\->(\w*)', 'SAMPLE'),
+            (r'(\w*)\-s\->(\w*)', 'SPLIT'),
+
+            (r'(\w*),(\w+)\-m\->(\w*)', 'MERGE'),
+            (r'(\w*),(\w+)\-c\->(\w*)', 'COMBINE'),
+
+            (r'(\w*)\-\(\+([\w,]+)\)\->(\w*)', 'PRESENCE_FILTER'),
+            (r'(\w*)\-\(\-([\w,]+)\)\->(\w*)', 'ABSENCE_FILTER'),
+
+            (r'([\w,]*)\-b(\d*)\->(\w*)', 'MULTI_BUFFER'),
+            (r'(\w*)\-\(([A-Za-z\_]*)\)\->(\w*)', 'MONITOR'),
+
+            # These commands are intended more for interactive use
+            (r'plot(\w*)', 'PLOT'),
+            (r'show(\w*)', 'SHOW'),
+            (r'unique(\w*)', 'UNIQUE'),
+            (r'hist(\d+),(\w*)', 'HISTOGRAM'),
+
+            #!! Save is to save droplets while load is to load scripts
+            (r'csv(\w*),(\w+)', 'SAVE'),
+            (r'(\w+)>(\w*)', 'LOAD'),
+
+            # Snippets have lowest precedence
+            (r'(\w+)', 'SNIPPET')
+
+        ]
+
+        if line:
+            self.parse_line(line)
+
+    def read_parse_loop(self, prompt='Stream> '):
+        "A prompt-read-parse_line loop."
+        print 'DropletStreams Interactive Session\nEnd session using quit or exit.\n'
+        while True:
+            inp = raw_input(prompt)
+            if inp == 'clear':
+                del self.streams
+                del self.droplets
+                self.streams = {}
+                self.droplets = {}
+
+            elif inp not in ('quit', 'exit'):
+                try:
+                    self.parse_line(inp)
+                except Exception, e:
+                    print 'Encountered Error:\n', repr(e)
+            else:
+                break
+
+    def load_file(self, filename):
+        f = open(filename, 'rb')
+
+        # Each line
+        print 'Executing script ' + filename
+
+        try:
+            for l in f.readlines():
+                self.parse_line(l)
+        finally:
+            f.close()
+
+    def parse_line(self, orig_line):
+        #Remove whitespace
+        lines = orig_line.replace(' ', '')
+
+        # Comment whole line
+        #if orig_line[0] == '#':
+        #    print 'comment'
+        #    return
+
+        # Split line into seperate lines marked by ;
+        lines = lines.split(';')
+
+        # Check first statement of line for special actions
+        start_patterns = [
+            (r'loop(\d+)', 'LOOP'),
+            (r'>(\w+)', 'SNIPPET')
+        ]
+
+        loop = 1
+        for pattern, tag in start_patterns:
+            regex = re.compile(pattern)
+            match = regex.match(lines[0])
+
+            if match:
+                lines.pop(0)
+                found = match.groups(0)[0]
+                if tag == 'LOOP':
+                    loop = int(found)
+                elif tag == 'SNIPPET':
+                    loop = 0
+                    self.snippets[found] = ';'.join(lines)
+                break
+
+        for i in range(loop):
+            for line in lines:
+                if len(line) == 0:
+                    continue
+                elif line[0] == '#':
+                    break
+
+                for pattern, tag in self.patterns:
+                    regex = re.compile(pattern)
+                    match = regex.match(line)
+                    if match:
+                        found = match.groups(0)
+                        if tag == 'CREATE_STREAM':
+                            origin, target = found
+                            params = origin.strip().split(',')
+
+                            if params == ['']:
+                                volume = 0
+                            else:
+                                volume = float(params[0])
+
+                            if len(params) == 2:
+                                volume_sigma = float(params[1])
+                            else:
+                                volume_sigma = 0.0
+
+                            if target == '':
+                                target = 'current'
+
+                            self.streams[target] = dr.stream(volume=volume, volume_sigma=volume_sigma)
+
+                        elif tag == 'ADD_CONTENT':
+                            params, target = found
+                            params = params.strip().split(',')
+                            if len(params) == 3:
+                                content, value, sigma = params
+                                sigma = float(sigma)
+                            else:
+                                content, value = params
+                                sigma = 0.0
+
+                            if target == '':
+                                target = 'current'
+
+                            original_stream = self.streams[target]
+                            new_stream = dr.stream({content: float(value)}, 0, content_sigma=sigma)
+
+                            self.streams[target] = dr.merge(original_stream, new_stream)
+
+                        elif tag == 'SAMPLE':
+                            orig, num, target = found
+
+                            if target == '':
+                                target = 'current'
+                            if orig == '':
+                                orig = 'current'
+
+                            self.droplets[target] = dr.sample(self.streams[orig], int(num))
+
+                        elif tag == 'SPLIT':
+                            orig, target = found
+
+                            if target == '':
+                                target = 'current'
+                            if orig == '':
+                                orig = 'current'
+
+                            self.streams[target] = dr.split(self.streams[orig])
+
+                        elif tag in ('MERGE', 'COMBINE'):
+                            orig1, orig2, target = found
+
+                            if target == '':
+                                target = 'current'
+                            if orig1 == '':
+                                orig1 = 'current'
+
+                            if tag == 'MERGE':
+                                self.streams[target] = dr.merge(self.streams[orig1], self.streams[orig2])
+
+                            elif tag == 'COMBINE':
+                                self.streams[target] = dr.combine(self.streams[orig1], self.streams[orig2])
+
+                        elif tag in ('PRESENCE_FILTER', 'ABSENCE_FILTER'):
+                            orig, contents, target = found
+                            contents = contents.split(',')
+
+                            if target == '':
+                                target = 'current'
+                            if orig == '':
+                                orig = 'current'
+
+                            if tag == 'PRESENCE_FILTER':
+                                func = dr.presence_filter(contents)
+                            else:
+                                func = dr.absence_filter(contents)
+
+                            self.streams[target] = dr.filter_stream(self.streams[orig], func)
+
+                        elif tag == 'MULTI_BUFFER':
+                            orig, capacity, target = found
+
+                            if capacity == '':
+                                capacity = 0
+                            else:
+                                capacity = int(capacity)
+
+                            # assemble streams
+                            streams = []
+                            for o in orig.split(','):
+                                if o == '':
+                                    o = 'current'
+                                streams.append(self.streams[o])
+
+                            if target == '':
+                                target = 'current'
+
+                            self.streams[target] = dr.multi_buffer(streams, capacity)
+
+                        elif tag == 'MONITOR':
+                            orig, name, target = found
+
+                            if not orig:
+                                orig = 'current'
+                            if not target:
+                                target = 'current'
+
+                            self.streams[target] = dr.droplet_monitor(self.streams[orig], name)
+
+                        elif tag == 'PLOT':
+                            orig = found[0]
+                            if orig == '':
+                                orig = 'current'
+
+                            try:
+                                dr.extract_data(self.droplets[orig], plot=True)
+                            except KeyError:
+                                raise RuntimeError('Could not find droplets: ' + orig)
+
+                        elif tag == 'SAVE':
+                            orig, filename = found
+                            if not orig:
+                                orig = 'current'
+                            filename += '.csv'
+
+                            dr.save_droplets(self.droplets[orig], filename)
+
+                        elif tag == 'LOAD':
+                            filename, snippet = found
+                            try:
+                                filename.index('.streams')
+                            except ValueError:
+                                filename += '.streams'
+
+                            if not snippet:
+                                # Execute file
+                                self.load_file(filename)
+                            else:
+                                # Save file as snippet
+                                f = open(filename, 'rb')
+                                try:
+                                    script = ''.join([l for l in f.read()])
+                                    self.snippets[snippet] = script
+                                finally:
+                                    f.close()
+
+                        elif tag == 'COPY':
+                            orig, target = found
+
+                            if not orig:
+                                orig = 'current'
+                            if not target:
+                                target = 'current'
+
+                            if not orig == target:
+                                self.streams[target] = dr.copy_stream(self.streams[orig])
+
+                        elif tag == 'SHOW':
+                            orig = found[0]
+
+                            if not orig:
+                                orig = 'current'
+
+                            repl = []
+
+                            if orig in self.streams.keys():
+                                repl.append('Stream {}: {}'.format(orig, self.streams[orig]))
+
+                            if orig in self.droplets.keys():
+                                d = self.droplets[orig]
+                                disp = '\n'.join(map(lambda x: 'Vol: {}, Cont: {}'.format(x[0], x[1]), d))
+
+                                repl.append('Droplets {}, amount: {}\n{}'.format(orig, len(d), disp))
+
+                            if orig in self.snippets.keys():
+                                repl.append('Snippet {}:\n{}'.format(orig, self.snippets[orig]))
+
+                            if not repl:
+                                repl.append('Could not find {} in streams or droplets.'.format(orig))
+
+                            print '\n\n'.join(repl)
+
+                        elif tag == 'UNIQUE':
+                            orig = found[0]
+                            if not orig:
+                                orig = 'current'
+
+                            dr.unique_droplets(self.droplets[orig])
+
+                        elif tag == 'HISTOGRAM':
+                            bins, orig = found
+
+                            if not orig:
+                                orig = 'current'
+
+                            if orig in self.droplets.keys():
+                                dr.histogram(self.droplets[orig], int(bins))
+
+                        elif tag == 'SNIPPET':
+                            snip = found[0]
+                            try:
+                                snippet = self.snippets[snip]
+                                for l in snippet.split('\n'):
+                                    self.parse_line(l)
+                            except KeyError:
+                                print 'Snippet not found.'
+
+                        # end pattern search after first match
+                        break
+
+
+if __name__ == '__main__':
+    s = StreamParser()
+    s.read_parse_loop()
